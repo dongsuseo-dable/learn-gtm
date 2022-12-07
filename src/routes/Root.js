c385864e610ff6036e995bd3c30cfd61cff574a6
dongsuseo-dable <115065754+dongsuseo-dable@users.noreply.github.com>
2022-12-07 11:09:13 +0900

    FEATURE: nav 이름나오도록 수정

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -86,6 +86,7 @@ export default function Root() {
                         : ""
                     }
                   >
+                    {contact.name}
                   </NavLink>
                 </li>
               ))}
